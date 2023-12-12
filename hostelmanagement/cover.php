f70137dfbb3fe0b0930579652cecc60ad9680734
Krishnams03 <Krishnams178@gmail.com>
2023-12-12 12:13:25 +0530

    commit

---
+++ hostelmanagement/cover.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cover Page</title>
+    <link rel="stylesheet" href="web_home/css_home/bootstrap.css">
+    <style>
+        /* Add your custom styles for the cover page here */
+        body {
+            background-color: #f8f9fa; /* Change to your preferred background color */
+        }
+        .cover-container {
+            text-align: center;
+            padding: 100px;
+        }
+        .cover-title {
+            font-size: 3em;
+            font-weight: bold;
+            color: #007bff; /* Change to your preferred text color */
+        }
+        .cover-description {
+            font-size: 1.5em;
+            color: #6c757d; /* Change to your preferred text color */
+        }
+    </style>
+    <link href="web_home/css_home/slider.css" type="text/css" rel="stylesheet" media="all">
+
+<!-- css files -->
+<link rel="stylesheet" href="web_home/css_home/bootstrap.css"> <!-- Bootstrap-Core-CSS -->
+<link rel="stylesheet" href="web_home/css_home/style.css" type="text/css" media="all"/> <!-- Style-CSS -->
+<link rel="stylesheet" href="web_home/css_home/fontawesome-all.css"> <!-- Font-Awesome-Icons-CSS -->
+<!-- //css files -->
+
+<!-- testimonials css -->
+<link rel="stylesheet" href="web_home/css_home/flexslider.css" type="text/css" media="screen" property="" /><!-- flexslider css -->
+<!-- //testimonials css -->
+
+<!-- web-fonts -->
+<link href="//fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i"
+    rel="stylesheet"> 	<!-- //web-fonts -->
+
+</head>
+<body>
+
+<div class="cover-container">
+    <h1 class="cover-title">Welcome to Shri Madhwa Vadiraja Institute of Technology and Management</h1>
+    <p class="cover-description">A Unit of Shri Sode Vadiraja Mutt Education Trust</p>
+    <a href="home.php" class="btn btn-primary btn-lg">Login</a>
+</div>
+
+</body>
+</html>
